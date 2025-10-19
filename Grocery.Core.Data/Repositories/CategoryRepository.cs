a3e21c818c89a10bd80b32a1f8fc8aff17d49e90
Beau Jean van Bemmel <beau.vanbemmel@rysst.nl>
2025-10-20 01:22:36 +0200

    Add Category to database

--- Grocery.Core.Data/Repositories/CategoryRepository.cs
+++ Grocery.Core.Data/Repositories/CategoryRepository.cs
@@ -1,29 +1,73 @@
 ﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
+using Microsoft.Data.Sqlite;
 
 namespace Grocery.Core.Data.Repositories
 {
-    public class CategoryRepository : ICategoryRepository
+    public class CategoryRepository : DatabaseConnection, ICategoryRepository
     {
-        private readonly List<Category> categories;
+        private readonly List<Category> categories = [];
         public CategoryRepository()
         {
-            categories = [
-                new Category(1, "Groente"),
-                new Category(2, "Bakkerij"),
-                new Category(3, "Zuivel"),
-                new Category(4, "Conserven"),
-                new Category(5, "Ontbijt")];
+            CreateTable(@"CREATE TABLE IF NOT EXISTS Category (
+                [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                [Name] NVARCHAR(40) NOT NULL UNIQUE
+            )");
+
+            List<string> insertQueries = [
+                @"INSERT OR IGNORE INTO Category (Name) VALUES ('Groente')",
+                @"INSERT OR IGNORE INTO Category (Name) VALUES ('Bakkerij')",
+                @"INSERT OR IGNORE INTO Category (Name) VALUES ('Zuivel')",
+                @"INSERT OR IGNORE INTO Category (Name) VALUES ('Conserven')",
+                @"INSERT OR IGNORE INTO Category (Name) VALUES ('Ontbijt')",
+			];
+
+            InsertMultipleWithTransaction(insertQueries);
+            GetAll();
         }
 
         public Category? Get(int id)
         {
-            return categories.FirstOrDefault(c => c.Id == id);
+			Category? category = null;
+			string selectQuery = $"SELECT Id, Name FROM Category Where Id = {id}";
+
+			OpenConnection();
+			using (SqliteCommand command = new(selectQuery, Connection))
+			{
+				SqliteDataReader reader = command.ExecuteReader();
+
+				if(reader.Read())
+				{
+					int _id = reader.GetInt32(0);
+					string name = reader.GetString(1);
+					category = new(id, name);
+				}
+			}
+
+			CloseConnection();
+			return category;
         }
 
         public List<Category> GetAll()
         {
-            return categories;
+			categories.Clear();
+			string selectQuery = "SELECT Id, Name FROM Category";
+
+			OpenConnection();
+			using (SqliteCommand command = new(selectQuery, Connection))
+			{
+				SqliteDataReader reader = command.ExecuteReader();
+
+				while (reader.Read())
+				{
+					int id = reader.GetInt32(0);
+					string name = reader.GetString(1);
+					categories.Add(new(id, name));
+				}
+			}
+
+			CloseConnection();
+			return categories;
         }
     }
 }
